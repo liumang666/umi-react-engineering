6b9c53c2187d219ddabe2303b5eab39286e15d46
liumang666 <1280060812@qq.com>
2023-02-24 23:28:24 +0800

    eslint commit 校验

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
+import React, { useState } from 'react';
 import styles from './index.less';
 
 export default function IndexPage() {
+  const [flag, setFlag] = useState(false);
+  const handleClick = () => {
+    setFlag(!flag);
+  };
+
+  const newFlag = flag ? '1' : '2';
+
+  if (flag) {
+    console.log(1111);
+  }
+
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title} onClick={handleClick}>
+        Page index
+      </h1>
+      <div>{newFlag}</div>
     </div>
   );
 }
